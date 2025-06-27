87aa5f60350325c0c4a841d28fe539a999baa7d1
barbara-foo-kaz <barbara@foo-kaz.com>
2025-06-27 10:45:41 +0200

    removed comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
 import "./globals.css";
-
-/* const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-}); */
 
 const playfair = Playfair_Display({ subsets: ["latin"], weight: ["400", "700"] });
 
